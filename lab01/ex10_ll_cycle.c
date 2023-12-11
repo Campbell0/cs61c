0abc0bc1e9865fe407fe48ef8774e36e65ca4c12
campbell <935703260@qq.com>
2023-12-11 10:24:43 +0800

    finish the task5 of proj1

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -2,5 +2,25 @@
 #include "ex10_ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    if(head == NULL) return 0;
+    node* fast_ptr = head;
+    node* slow_ptr = head;
+    while (fast_ptr != NULL) {
+        fast_ptr = fast_ptr->next;
+        if(fast_ptr != NULL) {
+            fast_ptr = fast_ptr->next;
+        }
+        else {
+            return 0;
+        }
+        if(fast_ptr == NULL) {
+            return 0;
+        }
+        slow_ptr = slow_ptr->next;
+        if (fast_ptr == slow_ptr)
+        {
+            return 1;
+        }
+    }
+    return 0;
 }
